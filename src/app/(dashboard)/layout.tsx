83a55ca5819d59cf368894791c32cbe24be7a590
Roni Faria <79719792+onnez7@users.noreply.github.com>
2025-05-30 15:22:11 -0400

    Optimize sidebar navigation

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { AppSidebar } from "@/components/app-sidebar";
+import { SiteHeader } from "@/components/site-header";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { Suspense } from "react";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SidebarProvider>
+      <AppSidebar variant="inset" />
+      <SidebarInset>
+        <SiteHeader />
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
